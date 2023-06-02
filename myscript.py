5a5b3d54f85eab3d8af0196f1f3f4837056122c5
Øivind Skjervheim <oskjervheim@gmail.com>
2023-06-02 21:17:26 +0200

    First attempt of communicatins with Spotifys API

---
+++ myscript.py
@@ -0,0 +1,61 @@
+
+
+import spotipy
+import json
+import webbrowser
+import MyCredentials
+
+oauth_object = spotipy.SpotifyOAuth(MyCredentials.clientID, MyCredentials.clientSecret, MyCredentials.redirect_uri)
+token_dict = oauth_object.get_access_token()
+token = token_dict['access_token']
+spotifyObject = spotipy.Spotify(auth=token)
+user_name = spotifyObject.current_user()
+  
+# To print the response in readable format.
+while True:
+    print("Welcome to the project, " + user_name['display_name'])
+    print("0 - Exit the console")
+    print("1 - Search for a Song")
+    user_input = int(input("Enter Your Choice: "))
+    if user_input == 1:
+        search_song = input("Enter the song name: ")
+        results = spotifyObject.search(search_song, 1, 0, "track")
+        songs_dict = results['tracks']
+        song_items = songs_dict['items']
+        song = song_items[0]['external_urls']['spotify']
+        webbrowser.open(song)
+        print('Song has opened in your browser.')
+    elif user_input == 0:
+        print("Good Bye, Have a great day!")
+        break
+    else:
+        print("Please enter valid user-input.")
+
+playlist_link = 'https://open.spotify.com/playlist/1co1URWkcdplr2iES6beRX?si=f90ba7b8c3574805'
+
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in spotifyObject.playlist_tracks(playlist_URI)["items"]]
+
+for track in spotifyObject.playlist_tracks(playlist_URI)["items"]:
+    #URI
+    track_uri = track["track"]["uri"]
+    
+    #Track name
+    track_name = track["track"]["name"]
+    
+    #Main Artist
+    artist_uri = track["track"]["artists"][0]["uri"]
+    artist_info = spotifyObject.artist(artist_uri)
+    
+    #Name, popularity, genre
+    artist_name = track["track"]["artists"][0]["name"]
+    artist_pop = artist_info["popularity"]
+    artist_genres = artist_info["genres"]
+    
+    #Album
+    album = track["track"]["album"]["name"]
+    
+    #Popularity of the track
+    track_pop = track["track"]["popularity"]
+
+#print(track_name)
